ca6851a77cd2655099410ae5686f074eee703624
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2019-10-22 17:08:56 -0500

    filter date added

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,83 +1,92 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
-
-//// Console.log the weather data from data.js
-//console.log(tableData);
-//
-//// Loop Through `tableData` and console.log each eye-witness report object
-//tableData.forEach(function(witnessReport) {
-    //console.log(witnessReport);
-  //});
-//
-//// Use d3 to append one table row `tr` for each eye-witness report object
-//tableData.forEach(function(witnessReport) {
-    //console.log(witnessReport);
-    //var row = tbody.append("tr");
-  //});
-// 
-//// Use `Object.entries` to console.log each eye-witness report value
-//tableData.forEach(function(witnessReport) {
-    //console.log(witnessReport);
-    //var row = tbody.append("tr");
-    //Object.entries(witnessReport).forEach(function([key, value]) {
-      //console.log(key, value);
-    //});
-  //});
-
-//// Use d3 to append 1 cell per eye-witness report value
-//tableData.forEach(function(witnessReport) {
-//    console.log(witnessReport);
-//    var row = tbody.append("tr");
-//    Object.entries(witnessReport).forEach(function([key, value]) {
-//      console.log(key, value);
-//      // Append a cell to the row for each value in the eye-witness report object
-//      var cell = row.append("td");
-//    });
-//  });
-
- //// Step 5: Use d3 to update each cell's text with
- //// weather report values (weekday, date, high, low)
- //tableData.forEach(function(witnessReport) {
-    //console.log(witnessReport);
-    //var row = tbody.append("tr");
-    //Object.entries(witnessReport).forEach(function([key, value]) {
-      //console.log(key, value);
-      //// Append a cell to the row for each value in the eye-witness report object
-      //var cell = row.append("td");
-      //cell.text(value);
-    //});
-  //});
-
   // Using Arrow Functions
-tableData.forEach((witnessReport) => {
+
+function allData() {
+  tbody.html("");
+  tableData.forEach((witnessReport) => {
     var row = tbody.append("tr");
     Object.entries(witnessReport).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
+}
 
-  var button = d3.select("#filter-btn");
+allData();
+var dateButton = d3.select("#date");
 
-  button.on("click", function() {
+var dates = tableData.map(witnessReport => witnessReport.datetime);
+let uniqueDates = Array.from(new Set(dates));
+console.log(uniqueDates)
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+uniqueDates.forEach((date) => {
+  var option = dateButton.append("option");
+  var valueDate = option.attr("value", date).text(date);
+});
 
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+//var date = dateButton.append("li");
+//date.append(value);
 
-    ("tbody tr").filter(report => report.datetime === inputValue);
-    // Then, select the table
-    //var list = tbody;
+//var button = d3.select("#placeholder");
+var inputElement = d3.select("#value");
+var searchButton = d3.select("#filter-btn")
 
-    //// remove any children from the list to
-    //tbody.html("");
+searchButton.on("click", function() {
+  console.log("Hi, a button was clicked!");
+  console.log(d3.event.target);
+});
+//function display (dataFiltered) {
+  //tbody.html("");
+    //dataFiltered.forEach((report) => {
+      //var row = tbody.append("tr");
+      //Object.entries(report).forEach(([key, value]) => {
+        //var cell = row.append("td");
+        //cell.text(value);
+      //})
+    //})
+  //};
+
+
+searchButton.on("click", function() {
+  console.log("button clicked");
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#date");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  if (inputValue === "All") {
+    allData();
+    console.log("All selected");
+  }
+  else {
+  var filtered = tableData.filter(report => report.datetime === inputValue);
+  tbody.html("");
+  filtered.forEach((report) => {
+    var row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    })
+  })
+}
+});
+  //// remove any children from the list to
+  //display (filtered);
+//});
+
+var dateList = d3.select("#list");
+
+//function click(event) {
+  //var value = d3.event.target.value;
+  //tbody.html("");
+  //var dateClick = tableData.filter(report => report.datetime === value);
+  //display (dateClick)
+//};
 //
-    //// append results to the list
-    //tbody.append(filteredData)
-  });
+//dateList.on("click", click);
+
+
